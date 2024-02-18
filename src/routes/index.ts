0575467f8d23ec4d66f17f9230d74729e971e3ee
MeChamaDeCarinha <backend.leonardo@gmail.com>
2024-02-18 00:18:30 -0300

    refactor: change response

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-    res.json("Hello World!");
+    return res.status(200).json("OK");
 });
 
 module.exports = router;
