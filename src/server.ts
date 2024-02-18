fda413e10e045abd5becc3cd38b66ea36a22307e
MeChamaDeCarinha <backend.leonardo@gmail.com>
2024-02-18 00:00:42 -0300

    feat: Initial commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+const port = 80;
+
+app.use(express.json());
+app.use(require("./routes/index"));
+
+
+app.listen(port, () => {
+    console.clear();
+    console.log("\n", "   \x1b[32m— JWT-Auth api\n", "   — Server started.", "\n\x1b[0m");
+});
